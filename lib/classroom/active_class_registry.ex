466c765c2860d1a54a96f7442c6bb5c4314232e2
herbert <sap02ckh2@yahoo.com.hk>
2018-11-16 21:35:45 +0800

    successfully added registry for active classes

--- lib/classroom/active_class_registry.ex
+++ lib/classroom/active_class_registry.ex
@@ -4,7 +4,6 @@ defmodule Classroom.ActiveClasses.Registry do
   #API
 
   def start_link do
-    IO.puts "testing from registry"
     GenServer.start_link(__MODULE__, nil, name: :registry)
   end
 
@@ -17,13 +16,13 @@ defmodule Classroom.ActiveClasses.Registry do
   end
 
   def unregister_name(class_name) do
-    GenServer.call(:registry, {:unregister_name, class_name})
+    GenServer.cast(:registry, {:unregister_name, class_name})
   end
 
   def send_name(class_name, message) do
     case whereis_name(class_name) do
-      nil ->
-        {:error, {class_name, message}}
+      :undefined ->
+        {:badarg, {class_name, message}}
 
       pid ->
         Kernel.send(pid, message)
@@ -38,18 +37,34 @@ defmodule Classroom.ActiveClasses.Registry do
   end
 
   def handle_call({:whereis_name, class_name}, _from, state) do
-    {:reply, Map.get(state, class_name), state}
+    {:reply, Map.get(state, class_name, :undefined), state}
   end
 
   def handle_call({:register_name, class_name, pid}, _from, state) do
     case Map.get(state, class_name) do
-      nil -> {:reply, :ok, Map.put(state, class_name, pid)}
-      _ -> {:reply, :error, state}
+      nil ->
+        Process.monitor(pid)
+        {:reply, :yes, Map.put(state, class_name, pid)}
+
+      _ ->
+        {:reply, :no, state}
     end
   end
 
-  def handle_call({:unregister_name, class_name}, _from, state) do # check whereis before unregister in user
-    {:reply, :ok, Map.delete(state, class_name)}
+  def handle_info({:DOWN, _, :process, pid, _}, state) do
+   # When a monitored process dies, we will receive a `:DOWN` message
+   # that we can use to remove the dead pid from our registry
+   {:noreply, remove_pid(state, pid)}
+  end
+
+  def remove_pid(state, pid_to_remove) do
+    # And here we just filter out the dead pid
+    remove = fn {_key, pid} -> pid  != pid_to_remove end
+    Enum.filter(state, remove) |> Enum.into(%{})
+  end
+
+  def handle_cast({:unregister_name, class_name}, state) do # check whereis before unregister in user
+    {:noreply, Map.delete(state, class_name)}
   end
 
 end
